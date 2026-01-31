9761f24d5a48862350802d8e9673d99fb36e0e08
modphoenix <ivanchenko.b@gmail.com>
2026-01-31 21:50:06 +0200

    Initial Commit

---
+++ whisper-on-deviceTests/whisper_on_deviceTests.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_on_deviceTests.swift
+//  whisper-on-deviceTests
+//
+//  Created by Bohdan Ivanchenko on 31.01.2026.
+//
+
+import Testing
+@testable import whisper_on_device
+
+struct whisper_on_deviceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
