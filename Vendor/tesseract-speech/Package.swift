e2b01bcbad046cd039a13780429d555e825f194b
spokvulcan <ivanchenko.b@gmail.com>
2026-08-23 22:44:01 +0200

    chore: bump mlx-swift pin to 24779d5 (lockstep with mlx-swift-lm)

--- Vendor/tesseract-speech/Package.swift
+++ Vendor/tesseract-speech/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(path: "../mlx-audio-swift"),
         .package(path: "../mlx-swift-lm"),
-        .package(url: "https://github.com/spokvulcan/mlx-swift", revision: "362e41efdc4ae9b4492fe531ec992e90a77d4f11"),
+        .package(url: "https://github.com/spokvulcan/mlx-swift", revision: "24779d5cf68ef0038ddd32caf6e1bd880d104e83"),
     ],
     targets: [
         .target(
